f7615c0930b9618106791ecf60d1dea39425599b
Shaheed mon <sheheedk07@gmail.com>
2024-11-01 14:49:55 +0530

    commit

--- todo/src/firebase/Crud.jsx
+++ todo/src/firebase/Crud.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect,useState } from 'react'
-import { db } from './Config'
+// import { db } from './Config'
 import {getDocs} from 'firebase/firestore'
 function Crud() {
-    const  [movieList,setMovieList]=useState([])
-   useEffect(()=>{
-    const getMovieList= async ()=>{
-        const data= await getDocs()
-    }
-   })
+    // const  [movieList,setMovieList]=useState([])
+  //  useEffect(()=>{
+    // const getMovieList= async ()=>{
+          // const data= await getDocs()
+    // }
+  //  })
   return (
     <div>
       
